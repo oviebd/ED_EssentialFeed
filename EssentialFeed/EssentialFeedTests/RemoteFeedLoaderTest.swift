f883bc347b4e509d6f30416d078e9a10ead55f6c
oviePeriscope <habibur.rahman@periscopelabs.io>
2024-10-03 06:01:37 +0600

    request data from url on load

--- EssentialFeed/EssentialFeedTests/RemoteFeedLoaderTest.swift
+++ EssentialFeed/EssentialFeedTests/RemoteFeedLoaderTest.swift
@@ -8,21 +8,33 @@
 import XCTest
 
 class RemoteFeedLoader{
-    
+    func load(){
+        HTTPClient.shared.requestedUrl = URL(string: "http://a-url.com")
+    }
 }
 
 class HTTPClient{
+    static let shared = HTTPClient()
+    private init(){}
     var requestedUrl : URL?
     
 }
 
 class RemoteFeedLoaderTest : XCTestCase{
     
-    func tesst_init_doesnotRequestDataFromURL(){
-        let client = HTTPClient()
+    func test_init_doesnotRequestDataFromURL(){
+        let client = HTTPClient.shared
         _ = RemoteFeedLoader()
         XCTAssertNil(client.requestedUrl)
     }
+    
+    func test_load_requestDataFromURL(){
+        let client = HTTPClient.shared
+           let sut = RemoteFeedLoader()
+           sut.load()
+           XCTAssertNotNil(client.requestedUrl)
+           
+       }
     
 }
 
