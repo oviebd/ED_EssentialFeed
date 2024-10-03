0e3180c115f8cfe830740823fd0612a984ad01c3
oviePeriscope <habibur.rahman@periscopelabs.io>
2024-10-03 10:00:44 +0600

    Delivers invalid data error on non 200 http response

--- EssentialFeed/EssentialFeedTests/RemoteFeedLoaderTest.swift
+++ EssentialFeed/EssentialFeedTests/RemoteFeedLoaderTest.swift
@@ -40,28 +40,51 @@ class RemoteFeedLoaderTest: XCTestCase {
         XCTAssertEqual(capturedErrors, [.connectivity])
     }
     
+    func test_load_deliverErrorOnNon200HTTPResponse(){
+        let (sut,client) = makeSUT()
+        var capturedErrors = [RemoteFeedLoader.Error]()
+    
+        sut.load(){ capturedErrors.append($0) }
+        let clientError = NSError(domain: "Test", code: 0)
+        client.complete(with: 400)
+    
+        XCTAssertEqual(capturedErrors, [.invalidData])
+    }
+    
     // Helpers
+   
     private func makeSUT(url : URL =  URL(string: "https://https://a-url.com")!) -> ( sut : RemoteFeedLoader, client : HTTPClientSpy){
         let client = HTTPClientSpy()
         let sut = RemoteFeedLoader(client: client, url: url)
         return (sut,client)
     }
     
+    
     private class HTTPClientSpy: HTTPClient {
       
-      
-        private var messages = [(url: URL, completion: (Error)-> Void)]()
+        private var messages = [(url: URL, completion: (Error?,HTTPURLResponse?)-> Void)]()
    
         var requestedUrls: [URL] {
             return messages.map{$0.url}
         }
         
-        func get(from url: URL, completion: @escaping(Error) -> Void) {
+        func get(from url: URL, completion: @escaping(Error?, HTTPURLResponse?) -> Void) {
             messages.append((url,completion))
         }
         
         func complete(with error : Error, at index : Int = 0){
-            messages[index].completion(error)
+            messages[index].completion(error,nil)
+        }
+        
+        func complete(with statusCode : Int, at index : Int = 0){
+            let response = HTTPURLResponse(
+                url: requestedUrls[index],
+                statusCode: statusCode,
+                httpVersion:nil,
+                headerFields: nil)
+            
+            messages[index].completion(nil,response)
+            
         }
     }
     
