0daea2d85f33922606b4953873a0db5d123223ac
oviePeriscope <habibur.rahman@periscopelabs.io>
2024-10-03 10:05:35 +0600

    Add more test assertion with different HTTP status code

--- EssentialFeed/EssentialFeedTests/RemoteFeedLoaderTest.swift
+++ EssentialFeed/EssentialFeedTests/RemoteFeedLoaderTest.swift
@@ -42,13 +42,19 @@ class RemoteFeedLoaderTest: XCTestCase {
     
     func test_load_deliverErrorOnNon200HTTPResponse(){
         let (sut,client) = makeSUT()
-        var capturedErrors = [RemoteFeedLoader.Error]()
-    
-        sut.load(){ capturedErrors.append($0) }
-        let clientError = NSError(domain: "Test", code: 0)
-        client.complete(with: 400)
-    
-        XCTAssertEqual(capturedErrors, [.invalidData])
+        
+        let samples = [199,201,300,400,500]
+        
+        samples.enumerated().forEach { index, code in
+           
+            var capturedErrors = [RemoteFeedLoader.Error]()
+            sut.load(){ capturedErrors.append($0) }
+            client.complete(with: code, at: index)
+        
+            XCTAssertEqual(capturedErrors, [.invalidData])
+        }
+        
+        
     }
     
     // Helpers
