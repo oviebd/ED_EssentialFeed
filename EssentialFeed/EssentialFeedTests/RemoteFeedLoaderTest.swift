7b58c595bb05a8d5e54bc39966a76591e7c41cc5
oviePeriscope <habibur.rahman@periscopelabs.io>
2024-10-03 06:30:32 +0600

    Refactor test to remove duplication

--- EssentialFeed/EssentialFeedTests/RemoteFeedLoaderTest.swift
+++ EssentialFeed/EssentialFeedTests/RemoteFeedLoaderTest.swift
@@ -26,27 +26,37 @@ protocol HTTPClient {
     func get(from url: URL)
 }
 
-class HTTPClientSpy: HTTPClient {
-    func get(from url: URL) {
-        requestedUrl = url
-    }
 
-    var requestedUrl: URL?
-}
 
 class RemoteFeedLoaderTest: XCTestCase {
     func test_init_doesnotRequestDataFromURL() {
-        let givenUrl : URL = URL(string: "https://https://a-url.com")!
+        let url : URL = URL(string: "https://https://a-url.com")!
         let client = HTTPClientSpy()
-        _ = RemoteFeedLoader(client: client, url: givenUrl)
+        _ = makeSUT(url: url)
         XCTAssertNil(client.requestedUrl)
     }
-
+    
     func test_load_requestDataFromURL() {
-        let givenUrl : URL = URL(string: "https://https://a-url.com")!
-        let client = HTTPClientSpy()
-        let sut = RemoteFeedLoader(client : client,url: givenUrl)
+        let url : URL = URL(string: "https://https://a-url.com")!
+        let (sut,client) = makeSUT(url: url)
         sut.load()
-        XCTAssertEqual(givenUrl,client.requestedUrl)
+        XCTAssertEqual(url,client.requestedUrl)
     }
+    
+    
+    // Helpers
+    private func makeSUT(url : URL =  URL(string: "https://https://a-url.com")!) -> ( sut : RemoteFeedLoader, client : HTTPClientSpy){
+        let client = HTTPClientSpy()
+        let sut = RemoteFeedLoader(client: client, url: url)
+        return (sut,client)
+    }
+    
+    private class HTTPClientSpy: HTTPClient {
+        var requestedUrl: URL?
+        
+        func get(from url: URL) {
+            requestedUrl = url
+        }
+    }
+    
 }
