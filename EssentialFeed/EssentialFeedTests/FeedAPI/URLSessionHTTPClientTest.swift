a773434ab90bf6d14394d55a872ce792814b0da5
oviePeriscope <habibur.rahman@periscopelabs.io>
2024-10-06 08:31:44 +0600

    Resume data task on creation

--- EssentialFeed/EssentialFeedTests/FeedAPI/URLSessionHTTPClientTest.swift
+++ EssentialFeed/EssentialFeedTests/FeedAPI/URLSessionHTTPClientTest.swift
@@ -17,7 +17,7 @@ class URLSessionHTTPClient{
     func get(from url: URL){
         session.dataTask(with: url){ _, _, _ in
             
-        }
+        }.resume()
     }
 }
 
@@ -31,20 +31,47 @@ final class URLSessionHTTPClientTest: XCTestCase {
         XCTAssertEqual([url], session.receivedURLs)
     }
     
+    func test_getFromURL_resumeDataTaskWithURL() {
+        let url = URL(string: "http://any_url.com")!
+        let session = URLSessionSpy()
+        let task = URLSessionDataTaskSpy()
+        session.stubs(url: url, task: task)
+        
+        let sut = URLSessionHTTPClient(session: session)
+        sut.get(from: url)
+        XCTAssertEqual(task.resumeCallcount, 1)
+    }
+    
+    
     
     
     //Mark - Helpers
     
     private class URLSessionSpy : URLSession{
         var receivedURLs = [URL]()
+        private var stubs = [URL:URLSessionDataTask]()
+        
+        func stubs(url : URL,task : URLSessionDataTask){
+            stubs[url] = task
+        }
         
         override func dataTask(with url: URL, completionHandler: @escaping (Data?, URLResponse?, Error?) -> Void) -> URLSessionDataTask {
             receivedURLs.append(url)
-            return FakeURLSessionDataTask()
+            return stubs[url] ?? FakeURLSessionDataTask()
         }
     }
     
     private class FakeURLSessionDataTask : URLSessionDataTask{
+        override func resume() {}
+    }
+   
+    private class URLSessionDataTaskSpy : URLSessionDataTask{
+        
+        var resumeCallcount = 0
+        override func resume() {
+            resumeCallcount += 1
+        }
+        
     }
     
 }
