ea7847d70270c39a3622f775e82f4857c9d133e6
oviePeriscope <habibur.rahman@periscopelabs.io>
2024-10-06 23:16:35 +0600

    Move URL creation to a shared factory method

--- EssentialFeed/EssentialFeedTests/FeedAPI/URLSessionHTTPClientTest.swift
+++ EssentialFeed/EssentialFeedTests/FeedAPI/URLSessionHTTPClientTest.swift
@@ -37,10 +37,8 @@ final class URLSessionHTTPClientTest: XCTestCase {
     
     func test_getFromURL_performGETRequestsWithURL() {
        
-        let url = URL(string: "http://any_url.com")!
-        
+        let url = anyURL()
         let exp = expectation(description: "Wait for request")
-        
         URLProtocolStub.observeRequests{ request in
             XCTAssertEqual(request.url,url)
             XCTAssertEqual(request.httpMethod,"GET")
@@ -54,13 +52,12 @@ final class URLSessionHTTPClientTest: XCTestCase {
     
     func test_getFromURL_failsOnRequestError() {
         
-        let url = URL(string: "http://any_url.com")!
         let error = NSError(domain: "any error", code: 1)
         URLProtocolStub.stub(data : nil, response : nil, error: error)
       
         let exp = expectation(description: "Wait for completion")
         
-        makeSut().get(from: url){ result in
+        makeSut().get(from: anyURL()){ result in
             switch result{
             case let .failure(receivedError as NSError):
                 XCTAssertEqual(receivedError.code, error.code)
@@ -84,6 +81,9 @@ final class URLSessionHTTPClientTest: XCTestCase {
         return sut
     }
     
+    private func anyURL()-> URL{
+        return URL(string: "http://any_url.com")!
+    }
     
     private class URLProtocolStub : URLProtocol {
         private static var stub : Stub?
