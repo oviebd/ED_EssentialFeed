5357fa7d2b08714adcfcf8795557ff25e7d2377d
oviePeriscope <habibur.rahman@periscopelabs.io>
2024-10-14 04:49:18 +0600

    Save command requests cache deletion

--- EssentialFeed/EssentialFeedTests/Feed Cache/CacheFeedUseCaseTests.swift
+++ EssentialFeed/EssentialFeedTests/Feed Cache/CacheFeedUseCaseTests.swift
@@ -5,11 +5,17 @@
 //  Created by Habibur Rahman on 14/10/24.
 //
 
+import EssentialFeed
 import XCTest
 
-class LocalFeedLoader{
-    init(store : FeedStore){
-        
+class LocalFeedLoader {
+    private let store: FeedStore
+    init(store: FeedStore) {
+        self.store = store
+    }
+
+    func save(_ items: [FeedItem]) {
+        store.deleteCacheFeedCallCount += 1
     }
 }
 
@@ -18,13 +24,29 @@ class FeedStore {
 }
 
 final class CacheFeedUseCaseTests: XCTestCase {
-
-    func test_init_doesnotDeleteCacheUponCreation(){
+    func test_init_doesnotDeleteCacheUponCreation() {
         let store = FeedStore()
         _ = LocalFeedLoader(store: store)
-        
+
         XCTAssertEqual(store.deleteCacheFeedCallCount, 0)
     }
-    
 
+    func test_save_requestsCacheDeletion() {
+        let store = FeedStore()
+        let sut = LocalFeedLoader(store: store)
+
+        let items = [uniqueItem(), uniqueItem()]
+        sut.save(items)
+        XCTAssertEqual(store.deleteCacheFeedCallCount, 1)
+    }
+
+    // MARK: - Helpers
+
+    private func uniqueItem() -> FeedItem {
+        return FeedItem(id: UUID(), description: "any", location: "any", imageURL: anyURL())
+    }
+
+    private func anyURL() -> URL {
+        return URL(string: "http://any-url.com")!
+    }
 }
