1a1895b364290efe24cdb080633f91a2890c7f01
oviePeriscope <habibur.rahman@periscopelabs.io>
2024-10-14 04:52:01 +0600

    Extract system under test creation to a factory method to protect tests against breaking changes

--- EssentialFeed/EssentialFeedTests/Feed Cache/CacheFeedUseCaseTests.swift
+++ EssentialFeed/EssentialFeedTests/Feed Cache/CacheFeedUseCaseTests.swift
@@ -25,15 +25,12 @@ class FeedStore {
 
 final class CacheFeedUseCaseTests: XCTestCase {
     func test_init_doesnotDeleteCacheUponCreation() {
-        let store = FeedStore()
-        _ = LocalFeedLoader(store: store)
-
+        let (_,store) = makeSUT()
         XCTAssertEqual(store.deleteCacheFeedCallCount, 0)
     }
 
     func test_save_requestsCacheDeletion() {
-        let store = FeedStore()
-        let sut = LocalFeedLoader(store: store)
+        let (sut,store) = makeSUT()
 
         let items = [uniqueItem(), uniqueItem()]
         sut.save(items)
@@ -41,6 +38,12 @@ final class CacheFeedUseCaseTests: XCTestCase {
     }
 
     // MARK: - Helpers
+    
+    private func makeSUT() -> (sut : LocalFeedLoader, store : FeedStore){
+        let store = FeedStore()
+        let sut = LocalFeedLoader(store: store)
+        return (sut,store)
+    }
 
     private func uniqueItem() -> FeedItem {
         return FeedItem(id: UUID(), description: "any", location: "any", imageURL: anyURL())
