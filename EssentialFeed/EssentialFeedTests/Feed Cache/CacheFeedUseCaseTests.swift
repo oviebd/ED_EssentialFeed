7fdc20b5bad5344cdeb04e43e105086fbe2fc774
oviePeriscope <habibur.rahman@periscopelabs.io>
2024-10-14 10:24:29 +0600

    Save command succeeds on successful cache insertion

--- EssentialFeed/EssentialFeedTests/Feed Cache/CacheFeedUseCaseTests.swift
+++ EssentialFeed/EssentialFeedTests/Feed Cache/CacheFeedUseCaseTests.swift
@@ -61,10 +61,13 @@ class FeedStore {
         insertionCompletions.append(completion)
         receivedMessages.append(.insert(items, timeStamp))
     }
-    
-    
+
     func completeInsertion(with error : Error, at index: Int = 0){
         insertionCompletions[index](error)
+    }
+    
+    func completeInsertionSuccessfully(at index: Int = 0){
+        insertionCompletions[index](nil)
     }
 }
 
@@ -135,6 +138,23 @@ final class CacheFeedUseCaseTests: XCTestCase {
         store.completeInsertion(with: insertionError)
         wait(for: [exp], timeout: 1.0)
         XCTAssertEqual(receivedError as NSError?,insertionError)
+    }
+    
+    func test_save_succeedsOnSuccessfulCacheInsertion(){
+      
+        let items = [uniqueItem(), uniqueItem()]
+        let (sut,store) = makeSUT()
+        
+        let exp = expectation(description: "wait for save completion")
+        var receivedError : Error?
+        sut.save(items){ error in
+            receivedError = error
+            exp.fulfill()
+        }
+        store.completeDeletionSuccessfully()
+        store.completeInsertionSuccessfully()
+        wait(for: [exp], timeout: 1.0)
+        XCTAssertNil(receivedError)
     }
 
     // MARK: - Helpers
