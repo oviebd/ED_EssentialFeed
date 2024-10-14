5c23fb9ab3a8adf4e45cc71b8fda4644430920be
oviePeriscope <habibur.rahman@periscopelabs.io>
2024-10-14 06:00:15 +0600

    Save command requests new cache insertion on successful deletion

--- EssentialFeed/EssentialFeedTests/Feed Cache/CacheFeedUseCaseTests.swift
+++ EssentialFeed/EssentialFeedTests/Feed Cache/CacheFeedUseCaseTests.swift
@@ -15,18 +15,37 @@ class LocalFeedLoader {
     }
 
     func save(_ items: [FeedItem]) {
-        store.deleteCacheFeed()
+        store.deleteCacheFeed{ [unowned self] error in
+            if error == nil{
+                self.store.insert(items)
+            }
+        }
     }
 }
 
 class FeedStore {
+    
+    typealias DeletionCompletion = (Error?) -> Void
+    
     var deleteCacheFeedCallCount = 0
     var insertCallCount = 0
-    func deleteCacheFeed(){
+    private var deletionCompletions = [DeletionCompletion]()
+    
+    func deleteCacheFeed(completion : @escaping DeletionCompletion){
         deleteCacheFeedCallCount += 1
+        deletionCompletions.append(completion)
     }
+    
     func completeDeletion(with error : Error, at index: Int = 0){
-        
+        deletionCompletions[index](error)
+    }
+    
+    func completeDeletionSuccessfully(at index: Int = 0){
+        deletionCompletions[index](nil)
+    }
+    
+    func insert(_ items : [FeedItem]){
+        insertCallCount += 1
     }
 }
 
@@ -50,6 +69,14 @@ final class CacheFeedUseCaseTests: XCTestCase {
         sut.save(items)
         store.completeDeletion(with: deletionerror)
         XCTAssertEqual(store.insertCallCount, 0)
+    }
+    
+    func test_save_requestNewCacheInsertationOnSuccessfulDeletion(){
+        let (sut,store) = makeSUT()
+        let items = [uniqueItem(), uniqueItem()]
+        sut.save(items)
+        store.completeDeletionSuccessfully()
+        XCTAssertEqual(store.insertCallCount, 1)
     }
 
     // MARK: - Helpers
