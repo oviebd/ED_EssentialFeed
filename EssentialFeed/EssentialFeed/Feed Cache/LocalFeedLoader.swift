b3be30a1451305f780270f3c7e8a24061325642a
oviePeriscope <habibur.rahman@periscopelabs.io>
2024-10-17 09:35:21 +0600

    Extract local members into properties

--- EssentialFeed/EssentialFeed/Feed Cache/LocalFeedLoader.swift
+++ EssentialFeed/EssentialFeed/Feed Cache/LocalFeedLoader.swift
@@ -10,7 +10,8 @@ import Foundation
 public final class LocalFeedLoader {
     private let store: FeedStore
     private let currentDate: () -> Date
-
+    private  let calender = Calendar(identifier: .gregorian)
+    
     public typealias SaveResult = Error?
     public typealias LoadResult = LoadFeedResult
     
@@ -48,6 +49,10 @@ public final class LocalFeedLoader {
         }
     }
 
+    private var maxCacheAgeInDays: Int {
+        return 7
+    }
+    
     private func cache(_ items: [FeedImage], with completion: @escaping (SaveResult) -> Void) {
         store.insert(items.toLocal(), timeStamp: currentDate()) { [weak self] error in
             guard self != nil else {
@@ -58,8 +63,8 @@ public final class LocalFeedLoader {
     }
     
     private func validate(_ timestamp : Date) -> Bool{
-        let calender = Calendar(identifier: .gregorian)
-        guard let maxCacheAge = calender.date(byAdding: .day, value: 7, to: timestamp) else{return false}
+       
+        guard let maxCacheAge = calender.date(byAdding: .day, value: maxCacheAgeInDays, to: timestamp) else{return false}
         return currentDate() < maxCacheAge
     }
 
