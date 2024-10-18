d76b494fcaf5554ecfb29676d8a02a1781ad4b89
oviePeriscope <habibur.rahman@periscopelabs.io>
2024-10-18 08:40:00 +0600

    Segment functionalities into extensions

--- EssentialFeed/EssentialFeed/Feed Cache/LocalFeedLoader.swift
+++ EssentialFeed/EssentialFeed/Feed Cache/LocalFeedLoader.swift
@@ -10,16 +10,27 @@ import Foundation
 public final class LocalFeedLoader {
     private let store: FeedStore
     private let currentDate: () -> Date
-    private  let calender = Calendar(identifier: .gregorian)
-    
+    private let calender = Calendar(identifier: .gregorian)
+
     public typealias SaveResult = Error?
     public typealias LoadResult = LoadFeedResult
-    
+
     public init(store: FeedStore, currentDate: @escaping () -> Date) {
         self.store = store
         self.currentDate = currentDate
     }
 
+    private var maxCacheAgeInDays: Int {
+        return 7
+    }
+
+    private func validate(_ timestamp: Date) -> Bool {
+        guard let maxCacheAge = calender.date(byAdding: .day, value: maxCacheAgeInDays, to: timestamp) else { return false }
+        return currentDate() < maxCacheAge
+    }
+}
+
+extension LocalFeedLoader {
     public func save(_ feed: [FeedImage], completion: @escaping (SaveResult) -> Void) {
         store.deleteCacheFeed { [weak self] error in
             guard let self = self else { return }
@@ -32,27 +43,39 @@ public final class LocalFeedLoader {
             }
         }
     }
-    
-    public func load(completion : @escaping (LoadResult) -> Void){
-        store.retrive{ [weak self] result in
-            
+
+    private func cache(_ items: [FeedImage], with completion: @escaping (SaveResult) -> Void) {
+        store.insert(items.toLocal(), timeStamp: currentDate()) { [weak self] error in
+            guard self != nil else {
+                return
+            }
+            completion(error)
+        }
+    }
+}
+
+extension LocalFeedLoader {
+    public func load(completion: @escaping (LoadResult) -> Void) {
+        store.retrive { [weak self] result in
+
             guard let self = self else { return }
-            
+
             switch result {
             case let .failure(error):
                 completion(.failure(error))
-                
+
             case let .found(feed, timestamp) where self.validate(timestamp):
                 completion(.success(feed.toModels()))
                 break
             case .found, .empty:
                 completion(.success([]))
-            
             }
         }
     }
-    
-    public func validateCache(){
+}
+
+extension LocalFeedLoader {
+    public func validateCache() {
         store.retrive(completion: { [weak self] result in
             guard let self = self else { return }
             switch result {
@@ -64,44 +87,19 @@ public final class LocalFeedLoader {
             case .empty, .found:
                 break
             }
-            
+
         })
-       
     }
-
-    private var maxCacheAgeInDays: Int {
-        return 7
-    }
-    
-    private func cache(_ items: [FeedImage], with completion: @escaping (SaveResult) -> Void) {
-        store.insert(items.toLocal(), timeStamp: currentDate()) { [weak self] error in
-            guard self != nil else {
-                return
-            }
-            completion(error)
-        }
-    }
-    
-    private func validate(_ timestamp : Date) -> Bool{
-       
-        guard let maxCacheAge = calender.date(byAdding: .day, value: maxCacheAgeInDays, to: timestamp) else{return false}
-        return currentDate() < maxCacheAge
-    }
-
 }
-
 
 private extension Array where Element == FeedImage {
     func toLocal() -> [LocalFeedImage] {
-        return map{LocalFeedImage(id: $0.id, description: $0.description, location: $0.location, imageURL: $0.url)}
-        
+        return map { LocalFeedImage(id: $0.id, description: $0.description, location: $0.location, imageURL: $0.url) }
     }
 }
 
 private extension Array where Element == LocalFeedImage {
     func toModels() -> [FeedImage] {
-        return map{FeedImage(id: $0.id, description: $0.description, location: $0.location, url: $0.url)}
-        
+        return map { FeedImage(id: $0.id, description: $0.description, location: $0.location, url: $0.url) }
     }
 }
-
