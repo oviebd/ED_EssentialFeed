734c9f52f2a03f1eb312249c5533fb7ec3c50b52
oviePeriscope <habibur.rahman@periscopelabs.io>
2024-10-19 11:41:38 +0600

    Add memory leak tracking

--- EssentialFeed/EssentialFeedTests/Feed Cache/CodableFeedStoreTests.swift
+++ EssentialFeed/EssentialFeedTests/Feed Cache/CodableFeedStoreTests.swift
@@ -144,7 +144,9 @@ final class CodableFeedStoreTests: XCTestCase {
     
     // - MARK: Helpers
     
-    func makeSUT() -> CodableFeedStore {
+    func makeSUT(file:StaticString = #file, line:  UInt = #line) -> CodableFeedStore {
+        let sut = CodableFeedStore()
+        trackForMemoryLeaks(sut,file: file,line: line)
         return CodableFeedStore()
     }
     
