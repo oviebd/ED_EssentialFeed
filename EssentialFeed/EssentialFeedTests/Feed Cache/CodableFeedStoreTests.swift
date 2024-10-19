147cd0dbe144ce198562011504c3495d4c8611c0
oviePeriscope <habibur.rahman@periscopelabs.io>
2024-10-19 12:37:09 +0600

    Retrieving from non-empty cache twice delivers same found result (no side-effects)

--- EssentialFeed/EssentialFeedTests/Feed Cache/CodableFeedStoreTests.swift
+++ EssentialFeed/EssentialFeedTests/Feed Cache/CodableFeedStoreTests.swift
@@ -141,6 +141,38 @@ final class CodableFeedStoreTests: XCTestCase {
         wait(for: [exp], timeout: 1)
     }
     
+    
+    func test_rerieve_hasNoSideffectsOnNonEmptyCache(){
+        let sut = makeSUT()
+        let feed = uniqueImageFeed().local
+        let timestamp = Date()
+        let exp = expectation(description: "Wait for cache cache retrieval")
+        sut.insert(feed, timeStamp: timestamp) { insertionError in
+            XCTAssertNil(insertionError, "Expected feed to be inserted successfully")
+            sut.retrive { firstResult in
+                sut.retrive { secondResult in
+                    switch (firstResult, secondResult) {
+                   
+                    case let (.found(firstFeed,firstTimestamp), .found(secondFeed,secondTimestamp)):
+                        XCTAssertEqual(firstFeed,feed)
+                        XCTAssertEqual(firstTimestamp,timestamp)
+                        
+                        XCTAssertEqual(secondFeed,feed)
+                        XCTAssertEqual(secondTimestamp,timestamp)
+                        
+                        break
+                    
+                    default:
+                        XCTFail("Expected retrieving twice from non empty cache to deliver same found result with feed \(feed) and timestamp \(timestamp), got \(firstResult) and \(secondResult) instead")
+                    }
+                    exp.fulfill()
+                }
+            }
+           
+        }
+        wait(for: [exp], timeout: 1)
+    }
+    
     // - MARK: Helpers
     
     private func makeSUT(file:StaticString = #file, line:  UInt = #line) -> CodableFeedStore {
