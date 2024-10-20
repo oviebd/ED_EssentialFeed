eee7476e98e4729ed593be0f2f3ff9121692d5f5
oviePeriscope <habibur.rahman@periscopelabs.io>
2024-10-20 15:56:29 +0600

    Make storeURL an explicit dependency

--- EssentialFeed/EssentialFeedTests/Feed Cache/CoreDataFeedStoreTests.swift
+++ EssentialFeed/EssentialFeedTests/Feed Cache/CoreDataFeedStoreTests.swift
@@ -5,69 +5,57 @@
 //  Created by Habibur Rahman on 20/10/24.
 //
 
-import XCTest
 import EssentialFeed
+import XCTest
 
-
-class CoreDataFeedStoreTests : XCTestCase, FeedStoreSpecs {
-   
+class CoreDataFeedStoreTests: XCTestCase, FeedStoreSpecs {
     func test_retrieve_deliversEmptyOnEmptyCache() {
         let sut = makeSUT()
         assertThatRetrieveDeliversEmptyOnEmptyCache(on: sut)
     }
-    
+
     func test_retrieve_hasNoSideEffectsOnEmptyCache() {
         let sut = makeSUT()
         assertThatRetrieveHasNoSideEffectsOnEmptyCache(on: sut)
     }
-    
+
     func test_retrieve_deliversFoundValuesOnNonEmptyCache() {
-        
     }
-    
+
     func test_retrieve_hasNoSideEffectsOnNonEmptyCache() {
-        
     }
-    
+
     func test_insert_deliversNoErrorOnEmptyCache() {
-        
     }
-    
+
     func test_insert_deliversNoErrorOnNonEmptyCache() {
-        
     }
-    
+
     func test_insert_overridesPreviouslyInsertedCacheValues() {
-        
     }
-    
+
     func test_delete_deliversNoErrorOnEmptyCache() {
-        
     }
-    
+
     func test_delete_hasNoSideEffectsOnEmptyCache() {
-        
     }
-    
+
     func test_delete_deliversNoErrorOnNonEmptyCache() {
-        
     }
-    
+
     func test_delete_emptiesPreviouslyInsertedCache() {
-        
     }
-    
+
     func test_storeSideEffects_runSerially() {
-        
     }
-    
+
     // - MARK: Helpers
-    
+
     private func makeSUT(file: StaticString = #file, line: UInt = #line) -> FeedStore {
         let storeBundle = Bundle(for: CoreDataFeedStore.self)
-        let sut = try! CoreDataFeedStore(bundle: storeBundle)
-        trackForMemoryLeaks(sut,file: file, line: line)
+        let storeURL = URL(fileURLWithPath: "/dev/null")
+        let sut = try! CoreDataFeedStore(storeURL: storeURL, bundle: storeBundle)
+        trackForMemoryLeaks(sut, file: file, line: line)
         return sut
     }
-    
 }
