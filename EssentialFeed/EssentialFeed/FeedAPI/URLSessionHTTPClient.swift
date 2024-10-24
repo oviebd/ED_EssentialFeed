83aba0936452de8dfc202fec34b5e09f5198b387
oviePeriscope <habibur.rahman@periscopelabs.io>
2024-10-24 11:29:56 +0600

    Simplify URLSessionHTTPClient completion code with the new Result

--- EssentialFeed/EssentialFeed/FeedAPI/URLSessionHTTPClient.swift
+++ EssentialFeed/EssentialFeed/FeedAPI/URLSessionHTTPClient.swift
@@ -7,7 +7,7 @@
 
 import Foundation
 
-public class URLSessionHTTPClient : HTTPClient {
+public class URLSessionHTTPClient: HTTPClient {
     private let session: URLSession
     public init(session: URLSession = .shared) {
         self.session = session
@@ -17,13 +17,18 @@ public class URLSessionHTTPClient : HTTPClient {
 
     public func get(from url: URL, completion: @escaping (HTTPClient.Result) -> Void) {
         session.dataTask(with: url) { data, response, error in
-            if let error = error {
-                completion(.failure(error))
-            } else if let data = data, let response = response as? HTTPURLResponse {
-                completion(.success((data, response)))
-            } else {
-                completion(.failure(UnexpectedValuesRepresentation()))
-            }
+
+            completion(Result {
+                if let error = error {
+                    throw error
+                } else if let data = data, let response = response as? HTTPURLResponse {
+                    return (data, response)
+                } else {
+                    throw (UnexpectedValuesRepresentation())
+                }
+
+            })
+
         }.resume()
     }
 }
