dcb373e302381a4a9170f74a1d2f1d53f6fbc5ee
Habibur Rahman <habibur.rahman@periscopelabs.io>
2024-11-11 10:23:21 +0600

    Extract duplicate base URL creation into a factory helper

--- EssentialFeed/EssentialFeedAPIEndToEndTests/LoadFeedFromRemoteUseCaseTests.swift
+++ EssentialFeed/EssentialFeedAPIEndToEndTests/LoadFeedFromRemoteUseCaseTests.swift
@@ -47,7 +47,7 @@ final class LoadFeedFromRemoteUseCaseTests: XCTestCase {
     private func getFeedResult(file: StaticString = #file, line: UInt = #line) -> FeedLoader.Result? {
         let testServerURL = URL(string: "https://essentialdeveloper.com/feed-case-study/test-api/feed")!
         let client = URLSessionHTTPClient(session: URLSession(configuration: .ephemeral))
-        let loader = RemoteFeedLoader(client: client, url: testServerURL)
+        let loader = RemoteFeedLoader(client: client, url: feedTestServerURL)
 
         trackForMemoryLeaks(client, file: file, line: line)
         trackForMemoryLeaks(loader, file: file, line: line)
@@ -66,7 +66,7 @@ final class LoadFeedFromRemoteUseCaseTests: XCTestCase {
     }
 
     private func getFeedImageDataResult(file: StaticString = #file, line: UInt = #line) -> FeedImageDataLoader.Result? {
-        let testServerURL = URL(string: "https://essentialdeveloper.com/feed-case-study/test-api/feed/73A7F70C-75DA-4C2E-B5A3-EED40DC53AA6/image")!
+        let url = feedTestServerURL.appendingPathComponent("73A7F70C-75DA-4C2E-B5A3-EED40DC53AA6/image")
         let client = URLSessionHTTPClient(session: URLSession(configuration: .ephemeral))
         let loader = RemoteFeedImageDataLoader(client: client)
         trackForMemoryLeaks(client, file: file, line: line)
@@ -75,7 +75,7 @@ final class LoadFeedFromRemoteUseCaseTests: XCTestCase {
         let exp = expectation(description: "Wait for load completion")
 
         var receivedResult: FeedImageDataLoader.Result?
-        _ = loader.loadImageData(from: testServerURL) { result in
+        _ = loader.loadImageData(from: url) { result in
             receivedResult = result
             exp.fulfill()
         }
@@ -134,4 +134,8 @@ final class LoadFeedFromRemoteUseCaseTests: XCTestCase {
     private func imageURL(at index: Int) -> URL {
         return URL(string: "https://url-\(index + 1).com")!
     }
+    
+    private var feedTestServerURL: URL {
+            return URL(string: "https://essentialdeveloper.com/feed-case-study/test-api/feed")!
+        }
 }
