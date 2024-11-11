655b9d73a50e5c8763d09a41034ea3e3211031ea
Habibur Rahman <habibur.rahman@periscopelabs.io>
2024-11-11 10:14:59 +0600

    Refactor switch statement with map and flatMap chain

--- EssentialFeed/EssentialFeed/FeedAPI/RemoteFeedImageDataLoader.swift
+++ EssentialFeed/EssentialFeed/FeedAPI/RemoteFeedImageDataLoader.swift
@@ -7,18 +7,18 @@
 
 import Foundation
 
-public final class RemoteFeedImageDataLoader : FeedImageDataLoader  {
+public final class RemoteFeedImageDataLoader: FeedImageDataLoader {
     private let client: HTTPClient
-    
-   public init(client: HTTPClient) {
+
+    public init(client: HTTPClient) {
         self.client = client
     }
-    
+
     public enum Error: Swift.Error {
         case connectivity
         case invalidData
     }
-    
+
     private final class HTTPClientTaskWrapper: FeedImageDataLoaderTask {
         private var completion: ((FeedImageDataLoader.Result) -> Void)?
 
@@ -27,37 +27,33 @@ public final class RemoteFeedImageDataLoader : FeedImageDataLoader  {
         init(_ completion: @escaping (FeedImageDataLoader.Result) -> Void) {
             self.completion = completion
         }
-        
+
         func complete(with result: FeedImageDataLoader.Result) {
             completion?(result)
         }
-        
+
         func cancel() {
             preventFurtherCompletions()
             wrapped?.cancel()
         }
-        
+
         private func preventFurtherCompletions() {
             completion = nil
         }
     }
-    
+
     @discardableResult
     public func loadImageData(from url: URL, completion: @escaping (FeedImageDataLoader.Result) -> Void) -> FeedImageDataLoaderTask {
         let task = HTTPClientTaskWrapper(completion)
         task.wrapped = client.get(from: url) { [weak self] result in
             guard self != nil else { return }
 
-            switch result {
-            case let .success(data, response):
-                if response.statusCode == 200, !data.isEmpty {
-                    task.complete(with: .success(data))
-                } else {
-                    task.complete(with: .failure(Error.invalidData))
-                }
-            case .failure: task.complete(with: .failure(Error.connectivity))
-            
-            }
+            task.complete(with: result
+                .mapError { _ in Error.connectivity }
+                .flatMap { data, response in
+                    let isValidResponse = response.statusCode == 200 && !data.isEmpty
+                    return isValidResponse ? .success(data) : .failure(Error.invalidData)
+                })
         }
         return task
     }
