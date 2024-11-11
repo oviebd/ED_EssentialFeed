42a6e7b09f9bec395cf94df1ff17fb2c1b3af0c3
Habibur Rahman <habibur.rahman@periscopelabs.io>
2024-11-11 09:21:41 +0600

    Extract duplicate anyData helper into a shared scope

--- EssentialFeed/EssentialFeedTests/Healper/SharedTestHelpers.swift
+++ EssentialFeed/EssentialFeedTests/Healper/SharedTestHelpers.swift
@@ -13,3 +13,7 @@ public func anyURL() -> URL {
 public func anyNSError() -> NSError {
     return NSError(domain: "any error", code: 1)
 }
+
+public func anyData() -> Data {
+    return Data("any data".utf8)
+}
