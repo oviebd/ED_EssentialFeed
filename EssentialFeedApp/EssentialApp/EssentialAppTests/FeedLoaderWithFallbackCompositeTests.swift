ca6b61b14a956a460128429d6ccca5d2ec603f7b
oviePeriscope <habibur.rahman@periscopelabs.io>
2024-11-14 12:04:48 +0600

    FeedImageDataLoaderWithFallbackComposite.loadImageData delivers err… …or on both primary and fallback loader failure

--- EssentialFeedApp/EssentialApp/EssentialAppTests/FeedLoaderWithFallbackCompositeTests.swift
+++ EssentialFeedApp/EssentialApp/EssentialAppTests/FeedLoaderWithFallbackCompositeTests.swift
@@ -110,6 +110,15 @@ final class FeedLoaderWithFallbackCompositeTests: XCTestCase {
                 fallbackLoader.complete(with: fallbackData)
             })
         }
+    
+    func test_loadImageData_deliversErrorOnBothPrimaryAndFallbackLoaderFailure() {
+            let (sut, primaryLoader, fallbackLoader) = makeSUT()
+
+            expect(sut, toCompleteWith: .failure(anyNSError()), when: {
+                primaryLoader.complete(with: anyNSError())
+                fallbackLoader.complete(with: anyNSError())
+            })
+        }
 
     // MARK: - Helpers
 
