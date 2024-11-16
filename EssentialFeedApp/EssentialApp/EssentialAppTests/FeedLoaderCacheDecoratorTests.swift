c6fcb8861b96ee47d05fb5ce10dc32b6ed8593ac
Habibur Rahman <habibur.rahman@periscopelabs.io>
2024-11-16 14:03:32 +0600

    Move FeedLoaderCacheDecorator to production

--- EssentialFeedApp/EssentialApp/EssentialAppTests/FeedLoaderCacheDecoratorTests.swift
+++ EssentialFeedApp/EssentialApp/EssentialAppTests/FeedLoaderCacheDecoratorTests.swift
@@ -7,26 +7,8 @@
 
 import EssentialFeed
 import XCTest
+import EssentialApp
 
-final class FeedLoaderCacheDecorator: FeedLoader {
-    private let decoratee: FeedLoader
-    private let cache: FeedCache
-
-    init(decoratee: FeedLoader, cache: FeedCache) {
-        self.decoratee = decoratee
-        self.cache = cache
-    }
-
-    func load(completion: @escaping (FeedLoader.Result) -> Void) {
-        decoratee.load { [weak self] result in
-
-            completion(result.map { feed in
-                self?.cache.save(feed) { _ in }
-                return feed
-            })
-        }
-    }
-}
 
 final class FeedLoaderCacheDecoratorTests: XCTestCase, FeedLoaderTestCase {
     func test_load_deliversFeedOnLoaderSuccess() {
