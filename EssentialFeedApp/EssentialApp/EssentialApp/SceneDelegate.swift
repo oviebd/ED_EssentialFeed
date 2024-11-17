27c2714ce18125cfb88cf19767392b03a687cfa9
oviePeriscope <habibur.rahman@periscopelabs.io>
2024-11-17 16:32:36 +0600

    Move DEBUG code paths from the main SceneDelegate to a new DebuggingS… …ceneDelegate subclass to separate debug- and test-specific code from production code

--- EssentialFeedApp/EssentialApp/EssentialApp/SceneDelegate.swift
+++ EssentialFeedApp/EssentialApp/EssentialApp/SceneDelegate.swift
@@ -12,6 +12,10 @@ import UIKit
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     var window: UIWindow?
 
+    let localStoreURL = NSPersistentContainer
+        .defaultDirectoryURL()
+        .appendingPathComponent("feed-store.sqlite")
+
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
         // Use this method to optionally configure and attach the UIWindow `window` to the provided UIWindowScene `scene`.
         // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
@@ -24,18 +28,9 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         let remoteFeedLoader = RemoteFeedLoader(client: remoteClient, url: remoteURL)
         let remoteImageLoader = RemoteFeedImageDataLoader(client: remoteClient)
 
-        let localStoreURL = NSPersistentContainer
-            .defaultDirectoryURL()
-            .appendingPathComponent("feed-store.sqlite")
         let localStore = try! CoreDataFeedStore(storeURL: localStoreURL)
         let localFeedLoader = LocalFeedLoader(store: localStore, currentDate: Date.init)
         let localImageLoader = LocalFeedImageDataLoader(store: localStore)
-
-        #if DEBUG
-            if CommandLine.arguments.contains("-reset") {
-                try? FileManager.default.removeItem(at: localStoreURL)
-            }
-        #endif
 
         window?.rootViewController = FeedUIComposer.feedComposedWith(
             feedLoader: FeedLoaderWithFallbackComposite(
@@ -50,25 +45,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
                     cache: localImageLoader)))
     }
 
-    private func makeRemoteClient() -> HTTPClient {
-        #if DEBUG
-            if UserDefaults.standard.string(forKey: "connectivity") == "offline" {
-                return AlwaysFailingHTTPClient()
-            }
-        #endif
+    func makeRemoteClient() -> HTTPClient {
         return URLSessionHTTPClient(session: URLSession(configuration: .ephemeral))
     }
 }
-
-#if DEBUG
-    private class AlwaysFailingHTTPClient: HTTPClient {
-        private class Task: HTTPClientTask {
-            func cancel() {}
-        }
-
-        func get(from url: URL, completion: @escaping (HTTPClient.Result) -> Void) -> HTTPClientTask {
-            completion(.failure(NSError(domain: "offline", code: 0)))
-            return Task()
-        }
-    }
-#endif
