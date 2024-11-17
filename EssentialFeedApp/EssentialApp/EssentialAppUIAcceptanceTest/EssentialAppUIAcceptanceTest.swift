fc0faf393be2993307bb316d34d107e812121141
Habibur Rahman <habibur.rahman@periscopelabs.io>
2024-11-17 06:58:41 +0600

    Add UI Test target for running high-level Acceptance Tests

---
+++ EssentialFeedApp/EssentialApp/EssentialAppUIAcceptanceTest/EssentialAppUIAcceptanceTest.swift
@@ -0,0 +1,20 @@
+//
+//  EssentialAppUIAcceptanceTest.swift
+//  EssentialAppUIAcceptanceTest
+//
+//  Created by Habibur Rahman on 17/11/24.
+//
+
+import XCTest
+
+final class EssentialAppUIAcceptanceTest: XCTestCase {
+
+    func test_onLaunch_displaysRemoteFeedWhenCustomerHasConnectivity() {
+            let app = XCUIApplication()
+
+            app.launch()
+
+            XCTAssertEqual(app.cells.count, 22)
+            XCTAssertEqual(app.cells.firstMatch.images.count, 1)
+        }
+}
