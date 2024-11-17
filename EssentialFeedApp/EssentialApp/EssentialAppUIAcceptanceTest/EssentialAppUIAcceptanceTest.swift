4b9af5673a3003e6cb29ffea5d72d58c51516543
oviePeriscope <habibur.rahman@periscopelabs.io>
2024-11-17 15:47:53 +0600

    Reset cache in every test run to make sure tests run in a clean state… … and dont influence the result of other tests.

--- EssentialFeedApp/EssentialApp/EssentialAppUIAcceptanceTest/EssentialAppUIAcceptanceTest.swift
+++ EssentialFeedApp/EssentialApp/EssentialAppUIAcceptanceTest/EssentialAppUIAcceptanceTest.swift
@@ -12,35 +12,36 @@ final class EssentialAppUIAcceptanceTest: XCTestCase {
         let app = XCUIApplication()
 
         app.launch()
-
+        app.launchArguments = ["-reset"]
         let feedCells = app.cells.matching(identifier: "feed-image-cell")
         XCTAssertEqual(feedCells.count, 22)
 
         let firstImage = app.images.matching(identifier: "feed-image-view").firstMatch
         XCTAssertTrue(firstImage.exists)
     }
-    
+
     func test_onLaunch_displaysCachedRemoteFeedWhenCustomerHasNoConnectivity() {
-            let onlineApp = XCUIApplication()
-            onlineApp.launch()
+        let onlineApp = XCUIApplication()
+        onlineApp.launch()
+        onlineApp.launchArguments = ["-reset"]
 
-            let offlineApp = XCUIApplication()
-            offlineApp.launchArguments = ["-connectivity", "offline"]
-            offlineApp.launch()
+        let offlineApp = XCUIApplication()
+        offlineApp.launchArguments = ["-connectivity", "offline"]
+        offlineApp.launch()
 
-            let cachedFeedCells = offlineApp.cells.matching(identifier: "feed-image-cell")
-            XCTAssertEqual(cachedFeedCells.count, 22)
+        let cachedFeedCells = offlineApp.cells.matching(identifier: "feed-image-cell")
+        XCTAssertEqual(cachedFeedCells.count, 22)
 
-            let firstCachedImage = offlineApp.images.matching(identifier: "feed-image-view").firstMatch
-            XCTAssertTrue(firstCachedImage.exists)
-        }
-    
+        let firstCachedImage = offlineApp.images.matching(identifier: "feed-image-view").firstMatch
+        XCTAssertTrue(firstCachedImage.exists)
+    }
+
     func test_onLaunch_displaysEmptyFeedWhenCustomerHasNoConnectivityAndNoCache() {
-            let app = XCUIApplication()
-            app.launchArguments = ["-reset", "-connectivity", "offline"]
-            app.launch()
+        let app = XCUIApplication()
+        app.launchArguments = ["-reset", "-connectivity", "offline"]
+        app.launch()
 
-            let feedCells = app.cells.matching(identifier: "feed-image-cell")
-            XCTAssertEqual(feedCells.count, 0)
-        }
+        let feedCells = app.cells.matching(identifier: "feed-image-cell")
+        XCTAssertEqual(feedCells.count, 0)
+    }
 }
