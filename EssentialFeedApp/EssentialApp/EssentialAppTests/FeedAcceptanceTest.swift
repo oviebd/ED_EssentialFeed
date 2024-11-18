1a84d1645e976acdee06a58371dc54cab2ef5a10
oviePeriscope <habibur.rahman@periscopelabs.io>
2024-11-18 21:27:27 +0600

    Displays empty feed on launch when customer has no connectivity and no cache

--- EssentialFeedApp/EssentialApp/EssentialAppTests/FeedAcceptanceTest.swift
+++ EssentialFeedApp/EssentialApp/EssentialAppTests/FeedAcceptanceTest.swift
@@ -33,6 +33,8 @@ class FeedAcceptanceTests: XCTestCase {
     }
 
     func test_onLaunch_displaysEmptyFeedWhenCustomerHasNoConnectivityAndNoCache() {
+        let feed = launch(httpClient: .offline, store: .empty)
+        XCTAssertEqual(feed.numberOfRenderedFeedImageViews(), 0)
     }
 
     // MARK: - Helpers
