50b09438eeb9855872e1ad83a8b3842679443abf
oviePeriscope <habibur.rahman@periscopelabs.io>
2024-11-19 20:34:23 +0600

    Record feed with failed image loading snapshot

--- EssentialFeed/EssentialFeediOSTests/EssentialFeediOSTests.swift
+++ EssentialFeed/EssentialFeediOSTests/EssentialFeediOSTests.swift
@@ -5,12 +5,11 @@
 //  Created by Habibur Rahman on 19/11/24.
 //
 
+@testable import EssentialFeed
 import EssentialFeediOS
 import XCTest
-@testable import EssentialFeed
 
 final class EssentialFeediOSTests: XCTestCase {
-    
     func test_emptyFeed() {
         let sut = makeSUT()
 
@@ -18,21 +17,29 @@ final class EssentialFeediOSTests: XCTestCase {
 
         record(snapshot: sut.snapshot(), named: "EMPTY_FEED")
     }
-    
+
     func test_feedWithContent() {
-            let sut = makeSUT()
-
-            sut.display(feedWithContent())
-
-            record(snapshot: sut.snapshot(), named: "FEED_WITH_CONTENT")
-        }
-    
-    func test_feedWithErrorMessage(){
         let sut = makeSUT()
-        
+
+        sut.display(feedWithContent())
+
+        record(snapshot: sut.snapshot(), named: "FEED_WITH_CONTENT")
+    }
+
+    func test_feedWithErrorMessage() {
+        let sut = makeSUT()
+
         sut.display(.error(message: "This is a \nmultiline error message"))
-        
+
         record(snapshot: sut.snapshot(), named: "FEED_WITH_ERROR_MESSAGE")
+    }
+
+    func test_feedWithFailedImageLoading() {
+        let sut = makeSUT()
+
+        sut.display(feedWithFailedImageLoading())
+
+        record(snapshot: sut.snapshot(), named: "FEED_WITH_FAILED_IMAGE_LOADING")
     }
 
     // MARK: - Helpers
@@ -48,21 +55,36 @@ final class EssentialFeediOSTests: XCTestCase {
     private func emptyFeed() -> [FeedImageCellController] {
         return []
     }
-    
+
     private func feedWithContent() -> [ImageStub] {
-            return [
-                ImageStub(
-                    description: "The East Side Gallery is an open-air gallery in Berlin. It consists of a series of murals painted directly on a 1,316 m long remnant of the Berlin Wall, located near the centre of Berlin, on Mühlenstraße in Friedrichshain-Kreuzberg. The gallery has official status as a Denkmal, or heritage-protected landmark.",
-                    location: "East Side Gallery\nMemorial in Berlin, Germany",
-                    image: UIImage.make(withColor: .red)
-                ),
-                ImageStub(
-                    description: "Garth Pier is a Grade II listed structure in Bangor, Gwynedd, North Wales.",
-                    location: "Garth Pier",
-                    image: UIImage.make(withColor: .green)
-                )
-            ]
-        }
+        return [
+            ImageStub(
+                description: "The East Side Gallery is an open-air gallery in Berlin. It consists of a series of murals painted directly on a 1,316 m long remnant of the Berlin Wall, located near the centre of Berlin, on Mühlenstraße in Friedrichshain-Kreuzberg. The gallery has official status as a Denkmal, or heritage-protected landmark.",
+                location: "East Side Gallery\nMemorial in Berlin, Germany",
+                image: UIImage.make(withColor: .red)
+            ),
+            ImageStub(
+                description: "Garth Pier is a Grade II listed structure in Bangor, Gwynedd, North Wales.",
+                location: "Garth Pier",
+                image: UIImage.make(withColor: .green)
+            ),
+        ]
+    }
+
+    private func feedWithFailedImageLoading() -> [ImageStub] {
+        return [
+            ImageStub(
+                description: nil,
+                location: "Cannon Street, London",
+                image: nil
+            ),
+            ImageStub(
+                description: nil,
+                location: "Brighton Seafront",
+                image: nil
+            ),
+        ]
+    }
 
     private func record(snapshot: UIImage, named name: String, file: StaticString = #file, line: UInt = #line) {
         guard let snapshotData = snapshot.pngData() else {
