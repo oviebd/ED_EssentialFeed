bf9a4b0763b87ecdfa53b0482330fcbfa259df1f
Habibur Rahman <habibur.rahman@periscopelabs.io>
2024-11-22 20:07:49 +0600

    Check isMainQueue instead of just isMainThread since there's no g… …uarantee that the main thread is running the main queue

--- EssentialFeedApp/EssentialApp/EssentialApp/CombineHelpers.swift
+++ EssentialFeedApp/EssentialApp/EssentialApp/CombineHelpers.swift
@@ -5,9 +5,9 @@
 //  Created by Habibur Rahman on 22/11/24.
 //
 
-import Foundation
 import Combine
 import EssentialFeed
+import Foundation
 
 public extension FeedImageDataLoader {
     typealias Publisher = AnyPublisher<Data, Error>
@@ -76,12 +76,24 @@ extension Publisher {
 
 extension DispatchQueue {
     static var immediateWhenOnMainQueueScheduler: ImmediateWhenOnMainQueueScheduler {
-        ImmediateWhenOnMainQueueScheduler()
+        ImmediateWhenOnMainQueueScheduler.shared
     }
 
     struct ImmediateWhenOnMainQueueScheduler: Scheduler {
         typealias SchedulerTimeType = DispatchQueue.SchedulerTimeType
         typealias SchedulerOptions = DispatchQueue.SchedulerOptions
+
+        static let shared = Self()
+        private static let key = DispatchSpecificKey<UInt8>()
+        private static let value = UInt8.max
+
+        private init() {
+            DispatchQueue.main.setSpecific(key: Self.key, value: Self.value)
+        }
+
+        private func isMainQueue() -> Bool {
+            DispatchQueue.getSpecific(key: Self.key) == Self.value
+        }
 
         var now: SchedulerTimeType {
             DispatchQueue.main.now
@@ -92,7 +104,7 @@ extension DispatchQueue {
         }
 
         func schedule(options: SchedulerOptions?, _ action: @escaping () -> Void) {
-            guard Thread.isMainThread else {
+            guard isMainQueue() else {
                 return DispatchQueue.main.schedule(options: options, action)
             }
 
