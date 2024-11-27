f2dac2ac0ee6f00d99c2646bb29ed5e5f0d8f68f
oviePeriscope <habibur.rahman@periscopelabs.io>
2024-11-27 08:19:49 +0600

    Remove unused test spy

--- EssentialFeed/EssentialFeedTests/Shared API/Helpers/HTTPClientSpy.swift
+++ EssentialFeed/EssentialFeedTests/Shared API/Helpers/HTTPClientSpy.swift
@@ -5,36 +5,36 @@
 //  Created by Habibur Rahman on 11/11/24.
 //
 
-import Foundation
-import EssentialFeed
-
- public class HTTPClientSpy: HTTPClient {
-    private struct Task: HTTPClientTask {
-        func cancel() {}
-    }
-
-    private var messages = [(url: URL, completion: (HTTPClient.Result) -> Void)]()
-
-    var requestedUrls: [URL] {
-        return messages.map { $0.url }
-    }
-
-     public func get(from url: URL, completion: @escaping (HTTPClient.Result) -> Void) -> HTTPClientTask {
-        messages.append((url, completion))
-        return Task()
-    }
-
-    func complete(with error: Error, at index: Int = 0) {
-        messages[index].completion(.failure(error))
-    }
-
-    func complete(with statusCode: Int, data: Data, at index: Int = 0) {
-        let response = HTTPURLResponse(
-            url: requestedUrls[index],
-            statusCode: statusCode,
-            httpVersion: nil,
-            headerFields: nil)!
-
-        messages[index].completion(.success((data, response)))
-    }
-}
+//import Foundation
+//import EssentialFeed
+//
+// public class HTTPClientSpy: HTTPClient {
+//    private struct Task: HTTPClientTask {
+//        func cancel() {}
+//    }
+//
+//    private var messages = [(url: URL, completion: (HTTPClient.Result) -> Void)]()
+//
+//    var requestedUrls: [URL] {
+//        return messages.map { $0.url }
+//    }
+//
+//     public func get(from url: URL, completion: @escaping (HTTPClient.Result) -> Void) -> HTTPClientTask {
+//        messages.append((url, completion))
+//        return Task()
+//    }
+//
+//    func complete(with error: Error, at index: Int = 0) {
+//        messages[index].completion(.failure(error))
+//    }
+//
+//    func complete(with statusCode: Int, data: Data, at index: Int = 0) {
+//        let response = HTTPURLResponse(
+//            url: requestedUrls[index],
+//            statusCode: statusCode,
+//            httpVersion: nil,
+//            headerFields: nil)!
+//
+//        messages[index].completion(.success((data, response)))
+//    }
+//}
