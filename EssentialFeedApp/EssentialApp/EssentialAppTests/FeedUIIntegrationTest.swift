727afdf7292dd84d371a1c784cf08b6be5ae1005
oviePeriscope <habibur.rahman@periscopelabs.io>
2024-11-29 08:31:38 +0600

    Replace "FEED_VIEW_CONNECTION_ERROR" key with "GENERIC_CONNECTION_ERROR"

--- EssentialFeedApp/EssentialApp/EssentialAppTests/FeedUIIntegrationTest.swift
+++ EssentialFeedApp/EssentialApp/EssentialAppTests/FeedUIIntegrationTest.swift
@@ -312,7 +312,7 @@ final class FeedUIIntegrationTest: XCTestCase {
         sut.loadViewIfNeeded()
 
         loader.completeFeedLoadingWithError(at: 0)
-        XCTAssertEqual(sut.errorMessage, localized("FEED_VIEW_CONNECTION_ERROR"))
+        XCTAssertEqual(sut.errorMessage, localized("GENERIC_CONNECTION_ERROR"))
         
         sut.simulateUserInitiatedFeedReload()
         XCTAssertEqual(sut.errorMessage, nil)
