69b5fd601a68fba8aa3806fb1229614264ddd165
oviePeriscope <habibur.rahman@periscopelabs.io>
2024-11-30 17:04:27 +0600

    cancel the cancellable

--- EssentialFeedApp/EssentialApp/EssentialApp/Composers/LoadResourcePresentationAdapter.swift
+++ EssentialFeedApp/EssentialApp/EssentialApp/Composers/LoadResourcePresentationAdapter.swift
@@ -53,6 +53,7 @@ extension LoadResourcePresentationAdapter : FeedImageCellControllerDelegate{
     
     func didCancelImageRequest() {
         cancellable?.cancel()
+        cancellable = nil
     }
 
 }
