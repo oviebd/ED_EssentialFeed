588a3aeb8f5dffa0910219a9c4f5f5d4afd4cea2
oviePeriscope <habibur.rahman@periscopelabs.io>
2024-12-04 09:48:04 +0600

    Implement Image Comments UI

--- EssentialFeed/EssentialFeediOS/Image Comments UI/Controllers/ImageCommentCellController.swift
+++ EssentialFeed/EssentialFeediOS/Image Comments UI/Controllers/ImageCommentCellController.swift
@@ -5,4 +5,27 @@
 //  Created by Habibur Rahman on 4/12/24.
 //
 
+import EssentialFeed
 import Foundation
+import UIKit
+
+public class ImageCommentCellController: CellController {
+    private let model: ImageCommentViewModel
+    public init(model: ImageCommentViewModel) {
+        self.model = model
+    }
+
+    public func view(in tableView: UITableView) -> UITableViewCell {
+        let cell: ImageCommentCell = tableView.dequeueReusableCell()
+        cell.messageLabel.text = model.message
+        cell.usernameLabel.text = model.username
+        cell.dateLabel.text = model.date
+        return cell
+    }
+
+    public func preload() {
+    }
+
+    public func cancelLoad() {
+    }
+}
