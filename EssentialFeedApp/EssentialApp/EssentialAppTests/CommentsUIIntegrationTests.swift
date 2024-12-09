2679813ddd47a9f7aff2e978e95c3c960b0c3eae
Habibur Rahman <habibur.rahman@periscopelabs.io>
2024-12-09 09:02:19 +0600

    Extract helper methods in separate files

---
+++ EssentialFeedApp/EssentialApp/EssentialAppTests/CommentsUIIntegrationTests.swift
@@ -0,0 +1,14 @@
+//
+//  CommentsUIIntegrationTests.swift
+//  EssentialAppTests
+//
+//  Created by Habibur Rahman on 9/12/24.
+//
+
+import XCTest
+
+final class CommentsUIIntegrationTests: XCTestCase {
+
+    
+
+}
