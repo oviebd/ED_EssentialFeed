09f6e896b1c0ef265982a69bf185a7990604e0a8
oviePeriscope <habibur.rahman@periscopelabs.io>
2024-12-22 15:57:02 +0600

    Set FeedEndpoint limit to 10

--- EssentialFeed/EssentialFeedTests/FeedAPI/FeedEndpointTests.swift
+++ EssentialFeed/EssentialFeedTests/FeedAPI/FeedEndpointTests.swift
@@ -12,11 +12,13 @@ class FeedEndpointTests: XCTestCase {
 
     func test_feed_endpointURL() {
         let baseURL = URL(string: "http://base-url.com")!
-
+        
         let received = FeedEndpoint.get.url(baseURL: baseURL)
-        let expected = URL(string: "http://base-url.com/v1/feed")!
-
-        XCTAssertEqual(received, expected)
+        
+        XCTAssertEqual(received.scheme, "http", "scheme")
+        XCTAssertEqual(received.host, "base-url.com", "host")
+        XCTAssertEqual(received.path, "/v1/feed", "path")
+        XCTAssertEqual(received.query, "limit=10", "query")
     }
 
 }
