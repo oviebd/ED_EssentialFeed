38b99a645c33aa347838e7b258231f1cf61afa05
oviePeriscope <habibur.rahman@periscopelabs.io>
2024-12-22 08:07:39 +0600

    Add "Load more" message label

--- EssentialFeed/EssentialFeediOS/Feed UI/Views/LoadMoreCell.swift
+++ EssentialFeed/EssentialFeediOS/Feed UI/Views/LoadMoreCell.swift
@@ -8,7 +8,6 @@
 import UIKit
 
 public class LoadMoreCell: UITableViewCell {
-
     private lazy var spinner: UIActivityIndicatorView = {
         let spinner = UIActivityIndicatorView(style: .medium)
         contentView.addSubview(spinner)
@@ -17,10 +16,30 @@ public class LoadMoreCell: UITableViewCell {
         NSLayoutConstraint.activate([
             spinner.centerXAnchor.constraint(equalTo: contentView.centerXAnchor),
             spinner.centerYAnchor.constraint(equalTo: contentView.centerYAnchor),
-            contentView.heightAnchor.constraint(greaterThanOrEqualToConstant: 40)
+            contentView.heightAnchor.constraint(greaterThanOrEqualToConstant: 40),
         ])
 
         return spinner
+    }()
+
+    private lazy var messageLabel: UILabel = {
+        let label = UILabel()
+        label.textColor = .tertiaryLabel
+        label.font = .preferredFont(forTextStyle: .footnote)
+        label.numberOfLines = 0
+        label.textAlignment = .center
+        label.adjustsFontForContentSizeCategory = true
+        contentView.addSubview(label)
+
+        label.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            label.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: 8),
+            contentView.trailingAnchor.constraint(equalTo: label.trailingAnchor, constant: 8),
+            label.topAnchor.constraint(equalTo: contentView.topAnchor, constant: 8),
+            contentView.bottomAnchor.constraint(equalTo: label.bottomAnchor, constant: 8),
+        ])
+
+        return label
     }()
 
     public var isLoading: Bool {
@@ -34,4 +53,8 @@ public class LoadMoreCell: UITableViewCell {
         }
     }
 
+    public var message: String? {
+        get { messageLabel.text }
+        set { messageLabel.text = newValue }
+    }
 }
