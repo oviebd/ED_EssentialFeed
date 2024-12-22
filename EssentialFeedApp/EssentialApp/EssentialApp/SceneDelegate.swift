0677948b692193d1c82876d9e67cdd2864cd446d
oviePeriscope <habibur.rahman@periscopelabs.io>
2024-12-22 16:01:25 +0600

    Add feed endpoint with "after_id" param

--- EssentialFeedApp/EssentialApp/EssentialApp/SceneDelegate.swift
+++ EssentialFeedApp/EssentialApp/EssentialApp/SceneDelegate.swift
@@ -79,7 +79,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     
     func makeLocalFeedLoaaderWithLocalFallback() -> AnyPublisher<Paginated<FeedImage>, Error>{
         
-        let url = FeedEndpoint.get.url(baseURL: baseURL)
+        let url = FeedEndpoint.get().url(baseURL: baseURL)
         //URL(string: "https://static1.squarespace.com/static/5891c5b8d1758ec68ef5dbc2/t/5db4155a4fbade21d17ecd28/1572083034355/essential_app_feed.json")!
 
       //  let remoteFeedLoader = httpClient.getPublisher(from: remoteURL).tryMap(FeedItemMapper.map)
