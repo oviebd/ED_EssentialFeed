a76967cf6d17186b44daf7fe216f61f5f7a2c534
oviePeriscope <habibur.rahman@periscopelabs.io>
2024-12-22 16:56:35 +0600

    Remove cell selection style

--- EssentialFeedApp/EssentialApp/EssentialAppTests/FeedAcceptanceTest.swift
+++ EssentialFeedApp/EssentialApp/EssentialAppTests/FeedAcceptanceTest.swift
@@ -5,10 +5,10 @@
 //  Created by Habibur Rahman on 18/11/24.
 //
 
-import XCTest
+@testable import EssentialApp
 import EssentialFeed
 import EssentialFeediOS
-@testable import EssentialApp
+import XCTest
 
 class FeedAcceptanceTests: XCTestCase {
     func test_onLaunch_displaysRemoteFeedWhenCustomerHasConnectivity() {
@@ -16,7 +16,25 @@ class FeedAcceptanceTests: XCTestCase {
 
         XCTAssertEqual(feed.numberOfRenderedFeedImageViews(), 2)
         XCTAssertEqual(feed.renderedFeedImageData(at: 0), makeImageData0())
+
         XCTAssertEqual(feed.renderedFeedImageData(at: 1), makeImageData1())
+        XCTAssertTrue(feed.canLoadMoreFeed)
+
+        feed.simulateLoadMoreFeedAction()
+
+        XCTAssertEqual(feed.numberOfRenderedFeedImageViews(), 3)
+        XCTAssertEqual(feed.renderedFeedImageData(at: 0), makeImageData0())
+        XCTAssertEqual(feed.renderedFeedImageData(at: 1), makeImageData1())
+        XCTAssertEqual(feed.renderedFeedImageData(at: 2), makeImageData2())
+        XCTAssertTrue(feed.canLoadMoreFeed)
+
+        feed.simulateLoadMoreFeedAction()
+
+        XCTAssertEqual(feed.numberOfRenderedFeedImageViews(), 3)
+        XCTAssertEqual(feed.renderedFeedImageData(at: 0), makeImageData0())
+        XCTAssertEqual(feed.renderedFeedImageData(at: 1), makeImageData1())
+        XCTAssertEqual(feed.renderedFeedImageData(at: 2), makeImageData2())
+        XCTAssertFalse(feed.canLoadMoreFeed)
     }
 
     func test_onLaunch_displaysCachedRemoteFeedWhenCustomerHasNoConnectivity() {
@@ -92,7 +110,7 @@ class FeedAcceptanceTests: XCTestCase {
         RunLoop.current.run(until: Date())
 
         let nav = feed.navigationController
-        //return nav?.topViewController as! ListViewController
+        // return nav?.topViewController as! ListViewController
         let comments = nav?.topViewController as! ListViewController
         comments.simulateAppearance()
         return comments
@@ -109,9 +127,16 @@ class FeedAcceptanceTests: XCTestCase {
             return makeImageData0()
         case "/image-1":
             return makeImageData1()
+        case "/image-2": return makeImageData2()
 
-        case "/essential-feed/v1/feed":
-            return makeFeedData()
+        case "/essential-feed/v1/feed" where url.query?.contains("after_id") == false:
+            return makeFirstFeedPageData()
+
+        case "/essential-feed/v1/feed" where url.query?.contains("after_id=A28F5FE3-27A7-44E9-8DF5-53742D0E4A5A") == true:
+            return makeSecondFeedPageData()
+
+        case "/essential-feed/v1/feed" where url.query?.contains("after_id=166FCDD7-C9F4-420A-B2D6-CE2EAFA3D82F") == true:
+            return makeLastEmptyFeedPageData()
 
         case "/essential-feed/v1/image/2AB2AE66-A4B7-4A16-B374-51BBAC8DB086/comments":
             return makeCommentsData()
@@ -123,12 +148,23 @@ class FeedAcceptanceTests: XCTestCase {
 
     private func makeImageData0() -> Data { UIImage.make(withColor: .red).pngData()! }
     private func makeImageData1() -> Data { UIImage.make(withColor: .green).pngData()! }
+    private func makeImageData2() -> Data { UIImage.make(withColor: .blue).pngData()! }
 
-    private func makeFeedData() -> Data {
+    private func makeFirstFeedPageData() -> Data {
         return try! JSONSerialization.data(withJSONObject: ["items": [
             ["id": "2AB2AE66-A4B7-4A16-B374-51BBAC8DB086", "image": "http://feed.com/image-0"],
-            ["id": "A28F5FE3-27A7-44E9-8DF5-53742D0E4A5A", "image": "http://feed.com/image-1"]
+            ["id": "A28F5FE3-27A7-44E9-8DF5-53742D0E4A5A", "image": "http://feed.com/image-1"],
         ]])
+    }
+
+    private func makeSecondFeedPageData() -> Data {
+        return try! JSONSerialization.data(withJSONObject: ["items": [
+            ["id": "166FCDD7-C9F4-420A-B2D6-CE2EAFA3D82F", "image": "http://feed.com/image-2"],
+        ]])
+    }
+
+    private func makeLastEmptyFeedPageData() -> Data {
+        return try! JSONSerialization.data(withJSONObject: ["items": []])
     }
 
     private func makeCommentsData() -> Data {
@@ -138,8 +174,8 @@ class FeedAcceptanceTests: XCTestCase {
                 "message": makeCommentMessage(),
                 "created_at": "2020-05-20T11:24:59+0000",
                 "author": [
-                    "username": "a username"
-                ]
+                    "username": "a username",
+                ],
             ],
         ]])
     }
