ad4188b865995e1026e6cbace5f016b182006744
Habibur Rahman <habibur.rahman@periscopelabs.io>
2024-12-23 10:28:31 +0600

    Optimize CoreData implementation to reduce cache misses

--- EssentialFeed/EssentialFeed/Feed Cache/CoreData/ManagedFeedImage.swift
+++ EssentialFeed/EssentialFeed/Feed Cache/CoreData/ManagedFeedImage.swift
@@ -8,31 +8,40 @@
 import CoreData
 
 extension ManagedFeedImage {
-    
     static func data(with url: URL, in context: NSManagedObjectContext) throws -> Data? {
-            return try first(with: url, in: context)?.data
-        }
-    
+        if let data = context.userInfo[url] as? Data { return data }
+        return try first(with: url, in: context)?.data
+    }
+
     internal static func images(from localFeed: [LocalFeedImage], in context: NSManagedObjectContext) -> NSOrderedSet {
-        return NSOrderedSet(array: localFeed.map { local in
+        let images = NSOrderedSet(array: localFeed.map { local in
             let managed = ManagedFeedImage(context: context)
             managed.id = local.id
             managed.imageDescription = local.description
             managed.location = local.location
             managed.url = local.url
+            managed.data = context.userInfo[local.url] as? Data
             return managed
         })
+        context.userInfo.removeAllObjects()
+        return images
     }
-    
+
     static func first(with url: URL, in context: NSManagedObjectContext) throws -> ManagedFeedImage? {
-            let request = NSFetchRequest<ManagedFeedImage>(entityName: entity().name!)
-            request.predicate = NSPredicate(format: "%K = %@", argumentArray: [#keyPath(ManagedFeedImage.url), url])
-            request.returnsObjectsAsFaults = false
-            request.fetchLimit = 1
-            return try context.fetch(request).first
-        }
+        let request = NSFetchRequest<ManagedFeedImage>(entityName: entity().name!)
+        request.predicate = NSPredicate(format: "%K = %@", argumentArray: [#keyPath(ManagedFeedImage.url), url])
+        request.returnsObjectsAsFaults = false
+        request.fetchLimit = 1
+        return try context.fetch(request).first
+    }
 
     internal var local: LocalFeedImage {
         return LocalFeedImage(id: id!, description: imageDescription, location: location, imageURL: url!)
+    }
+
+    override public func prepareForDeletion() {
+        super.prepareForDeletion()
+
+        managedObjectContext?.userInfo[url] = data
     }
 }
